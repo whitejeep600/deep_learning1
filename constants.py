e0fd897e7c5971c35f3d68893a055862efda02e7
antoni.maciag1@gmail.com <antoni.maciag1@gmail.com>
2022-10-09 11:28:23 +0800

    data

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 INTENT_CKPT_DIRECTORY = './ckpt/intent/'
 SLOT_CKPT_DIRECTORY = './ckpt/slot/'
-INTENT_DATA_DIRECTORY = './ckpt/intent/'
-SLOT_DATA_DIRECTORY = './ckpt/slot/'
+INTENT_DATA_DIRECTORY = './data/intent/'
+SLOT_DATA_DIRECTORY = './data/slot/'
 INTENT_CACHE_DIRECTORY = './cache/intent/'
 SLOT_CACHE_DIRECTORY = './cache/slot/'
 
