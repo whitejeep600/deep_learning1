b6c1286265766ff86e7784690ffc816529804a8f
antoni.maciag1@gmail.com <antoni.maciag1@gmail.com>
2022-10-10 21:18:29 +0800

    final maybe

--- test_intent.py
+++ test_intent.py
@@ -5,6 +5,7 @@ from testers import IntentTester
 
 if __name__ == "__main__":
     args = parse_test_args("./data/intent/test.json", "./cache/intent/", INTENT_CKPT_DIRECTORY, "pred.intent.csv")
+    args.ckpt_dir.mkdir(parents=True, exist_ok=True)
     tester = IntentTester(args.cache_dir / "intent2idx.json", SeqClsTestDataset, args.max_len, args.batch_size,
                           args.cache_dir, args.test_file, args.ckpt_dir, args.pred_file)
     tester.test()
--- test_slot.py
+++ test_slot.py
@@ -5,6 +5,7 @@ from testers import SlotTester
 
 if __name__ == "__main__":
     args = parse_test_args("./data/slot/test.json", "./cache/slot/", SLOT_CKPT_DIRECTORY, "pred.slot.csv")
+    args.ckpt_dir.mkdir(parents=True, exist_ok=True)
     tester = SlotTester(args.cache_dir / "tag2idx.json", SeqTaggingClsTestDataset, args.max_len, args.batch_size,
                         args.cache_dir, args.test_file, args.ckpt_dir, args.pred_file)
     tester.test()
